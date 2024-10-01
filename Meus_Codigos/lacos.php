71a0170f4152212aec120f6b2ecd8596c3d333e2
Wesley schuab Vieira <58818125+WesleySchuab@users.noreply.github.com>
2024-10-01 15:48:18 -0300

    for

--- Meus_Codigos/lacos.php
+++ Meus_Codigos/lacos.php
@@ -6,3 +6,9 @@ while ($contador <= 15) {
     echo "#$contador" . PHP_EOL;
     $contador = $contador + 1;
 }
+
+<?php
+
+for ($contador = 1; $contador <= 15; $contador++) {
+    echo "#$contador" . PHP_EOL;
+}
